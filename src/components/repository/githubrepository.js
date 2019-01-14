c576bfc119409037301f7847c3a7acef6900dec6
Chahid hanae <hanae.chahid@gmail.com>
2019-01-14 20:53:52 +0100

    Number of stars and issues fixed

--- src/components/repository/githubrepository.js
+++ src/components/repository/githubrepository.js
@@ -4,9 +4,9 @@ import './repository.css';
 const GithubRepository = (props) => {
     const { repository} = props;
     //Number of stars
-    const stars = repository.stargazers_count > 999 ?  (repository.stargazers_count/ 1000).toFixed(1) : repository.stargazers_count;
+    const stars = repository.stargazers_count > 999 ?  (repository.stargazers_count/ 1000).toFixed(1)+'k' : repository.stargazers_count;
     //number of issues
-    const issues = repository.open_issues_count > 999 ? (repository.open_issues_count / 1000).toFixed(1) : repository.open_issues_count;
+    const issues = repository.open_issues_count > 999 ? (repository.open_issues_count / 1000).toFixed(1)+'k' : repository.open_issues_count;
     const dateSubmited = Date.parse(new Date())  - Date.parse(repository.created_at);
     const NDays = Math.floor(dateSubmited / (1000*60*60*24));
     return (
@@ -21,8 +21,8 @@ const GithubRepository = (props) => {
                             <h1>{repository.name}</h1>
                                 <p>{repository.description}</p>
                             <div className="stars-issues">
-                                <p className="stars-issues-border">Stars: {stars}K</p>
-                                <p className="stars-issues-border">Issues: {issues}K</p>
+                                <p className="stars-issues-border">Stars: {stars}</p>
+                                <p className="stars-issues-border">Issues: {issues}</p>
                                 <p> Submited {NDays } days ago by {repository.owner.login}</p>
                             </div>
                         </div>
